620a240a67d261581af621423ff08297f3f607c1
smilejakdu <ash982416@gmail.com>
2024-05-01 10:29:58 +0900

    first commit

---
+++ src/main/java/com/example/coupon_server/CouponServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupon_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponServerApplication.class, args);
+    }
+
+}
